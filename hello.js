d03f6f016efff3e63b7e798bdc9d454ef61461cf
tajjav <taj.jav@hotmail.com>
2023-08-07 18:18:58 -0600

    updated to include return

--- hello.js
+++ hello.js
@@ -6,9 +6,24 @@
 
 
 
-const sayHello  = function (name) {
-  console.log("Hello, " + name);
+// const sayHello = function (name) {
+//   console.log("Hello, " + name);
+// }
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+
+
+// const sayHelloToConsole  = function (name) {
+//   console.log("Hello, " + name);
+// }
+// sayHelloToConsole('John'); 
+
+
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
 }
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+const greeting = returnSayHello('John');
+console.log(greeting);
